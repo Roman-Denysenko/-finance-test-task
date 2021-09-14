8eb0e4dfe757cfc22c8baf9691ab888d29066182
Roman-Denysenko <roman.denisenko83@gmail.com>
2021-09-14 15:24:19 +0300

    Add config.js

---
+++ src/redux/tickers/config.js
@@ -0,0 +1,3 @@
+export const URL_WS = 'http://localhost:4000/';
+export const START_WS = 'start';
+export const TICKER_WS = 'ticker';
--- src/redux/tickers/operations.js
+++ src/redux/tickers/operations.js
@@ -1,13 +1,12 @@
 import { io } from 'socket.io-client';
 
 import { getTickers } from './actions';
-
-const url = 'http://localhost:4000/';
+import { URL_WS, START_WS, TICKER_WS } from './config';
 
 export const socketConnect = () => dispatch => {
-  const socket = io.connect(url);
-  socket.emit('start');
-  socket.on('ticker', function (response) {
+  const socket = io.connect(URL_WS);
+  socket.emit(START_WS);
+  socket.on(TICKER_WS, function (response) {
     const res = Array.isArray(response) ? response : [response];
     dispatch(getTickers(res));
   });
